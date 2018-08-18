de0372ae49c9d6c868b5e35fc7d3ef4ea3861c90
Marvyn Zalewski <mszalewski@ownpixel.com>
2018-08-18 13:54:26 +0200

    updated code to use new rbtv api; cleaned up code; implemented further error handling and signaling

--- src/main.cpp
+++ src/main.cpp
@@ -6,21 +6,23 @@
 /*
 * Coniguration
 ** CONNECTION_INDICATOR - blinking led during startup when defined
+** ERROR_INDICATOR - blinking led sequence when parsing or http error occured
 */
 #define CONNECTION_INDICATOR
+#define ERROR_INDICATOR
 #define LED_RED GPIO_NUM_14
 #define LED_GREEN GPIO_NUM_27
 #define LED_BLUE GPIO_NUM_26
 
-#define WIFI_SSID "WLAN"
-#define WIFI_PASSWORD "WLAN_PASSWORD"
-#define ROCKETBEANS_API "https://www.rocketbeans.tv/?next5Shows=true"
+#define WIFI_SSID "Your Wifi"
+#define WIFI_PASSWORD "Your Wifi Password"
+#define ROCKETBEANS_API "https://api.rocketbeans.tv/v1/frontend/init"
 
 /*
 * Information
 */
 #define __product__ "Beanlight"
-#define __version__ "1000DEV"
+#define __version__ "1001DEV"
 #define __author__ "Marvyn Zalewski <mszalewski@ownpixel.com>"
 #define __copyright__ "(c) 2018 KeyboardInterrupt.org"
 
@@ -32,6 +34,34 @@ int status = WL_IDLE_STATUS;
 /*
 * Functions
 */
+void setLampToBlack()
+{
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, LOW);
+}
+
+void setLampToBlue()
+{
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, HIGH);
+}
+
+void setLampToGreen()
+{
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, LOW);
+}
+
+void setLampToCyan()
+{
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+}
+
 void setLampToRed()
 {
     digitalWrite(LED_RED, HIGH);
@@ -39,32 +69,25 @@ void setLampToRed()
     digitalWrite(LED_BLUE, LOW);
 }
 
-void setLampToBlue()
+void setLampToPurple()
 {
-    digitalWrite(LED_BLUE, HIGH);
+    digitalWrite(LED_RED, HIGH);
     digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_BLUE, HIGH);
 }
 
-void setLampToGreen()
+void setLampToYellow()
 {
+    digitalWrite(LED_RED, HIGH);
     digitalWrite(LED_GREEN, HIGH);
     digitalWrite(LED_BLUE, LOW);
-    digitalWrite(LED_RED, LOW);
 }
 
 void setLampToWhite()
 {
+    digitalWrite(LED_RED, HIGH);
     digitalWrite(LED_GREEN, HIGH);
     digitalWrite(LED_BLUE, HIGH);
-    digitalWrite(LED_RED, HIGH);
-}
-
-void setLampToBlack()
-{
-    digitalWrite(LED_GREEN, LOW);
-    digitalWrite(LED_BLUE, LOW);
-    digitalWrite(LED_RED, LOW);
 }
 
 /*
@@ -116,28 +139,85 @@ void loop()
         int httpCode = http.GET();
         if (httpCode > 0)
         {
-            const size_t bufferSize = JSON_ARRAY_SIZE(5) + 5 * JSON_OBJECT_SIZE(14) + 2970;
+            const size_t bufferSize = JSON_ARRAY_SIZE(4) + JSON_ARRAY_SIZE(8) + 2 * JSON_OBJECT_SIZE(2) + 10 * JSON_OBJECT_SIZE(3) + 5 * JSON_OBJECT_SIZE(4) + JSON_OBJECT_SIZE(8) + 1190;
             DynamicJsonBuffer jsonBuffer(bufferSize);
             String payload = http.getString();
-            JsonArray &root = jsonBuffer.parseArray(payload);
+            JsonObject &root = jsonBuffer.parseObject(payload);
+            JsonObject &data = root["data"];
+            JsonObject &data_streamInfo = data["streamInfo"];
+            JsonObject &data_streamInfo_info = data_streamInfo["info"];
+            String data_streamInfo_info_type = data_streamInfo_info["type"].as<String>();
+
             if (!root.success())
             {
                 Serial.println((String)__product__ + " parsing failed.");
                 Serial.println(payload);
+                #ifdef ERROR_INDICATOR
+                for (int i = 0; i <= 6; i++)
+                {
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        setLampToCyan();
+                        delay(100);
+                        setLampToBlack();
+                        delay(100);
+                    };
+                    delay(500);
+                }
+                #endif
+                setLampToCyan();
+            } else
+            {
+                /* When current show is live */
+                if (data_streamInfo_info_type == "live")
+                    setLampToRed();
+                /* When current show is new */
+                else if (data_streamInfo_info_type == "premiere")
+                    setLampToBlue();
+                /* When current show is playback */
+                else if (data_streamInfo_info_type == "rerun")
+                    setLampToWhite();
+                /* Graceful handling if no type matched */
+                else {
+                    Serial.println((String)__product__ + " unsupported show type -> "+ data_streamInfo_info_type +".");
+                    #ifdef ERROR_INDICATOR
+                    for (int i = 0; i <= 3; i++)
+                    {
+                        for (int i = 0; i <= 3; i++)
+                        {
+                            setLampToPurple();
+                            delay(100);
+                            setLampToBlack();
+                            delay(100);
+                        };
+                        delay(500);
+                    }
+                    #endif
+                    setLampToPurple();
+                }
             }
-            /* When current show is live */
-            if (root[0]["isLive"] == 1)
-                setLampToRed();
-            /* When current show is new */
-            else if (root[0]["isNew"] == 1)
-                setLampToBlue();
-            /* When current show is playback */
-            else
-                setLampToWhite();
         }
         else
         {
             Serial.println((String)__product__ + " error on HTTP request");
+            #ifdef ERROR_INDICATOR
+            for (int i = 0; i <= 3; i++)
+            {
+                for (int i = 0; i <= 3; i++)
+                {
+                    setLampToRed();
+                    delay(100);
+                    setLampToBlack();
+                    delay(100);
+                    setLampToYellow();
+                    delay(100);
+                    setLampToBlack();
+                    delay(100);
+                };
+                delay(500);
+            }
+            #endif
+            setLampToYellow();
         }
         http.end();
     }
